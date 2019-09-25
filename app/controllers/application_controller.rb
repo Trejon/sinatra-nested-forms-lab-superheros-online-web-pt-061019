a01066f2ba7525244a6da4b251adf6b7cca728ba
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-09-25 03:17:07 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,10 @@ class App < Sinatra::Base
     @hero_power = [] 
     @hero_bio = [] 
     @team_members = params[:team][:members]
+    @team_members.each do |hero|
+      @hero_name << hero[:name]
+      @hero_power << hero[:power]
+      @hero_bio << hero[:bio]
     
   end 
   erb :team
